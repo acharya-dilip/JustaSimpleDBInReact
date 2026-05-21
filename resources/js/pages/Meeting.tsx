2cd7856337bcc8c8f3697e7a3ac4d36c9dc954c7
acharya-dilip <acharyamission6@gmail.com>
2026-05-21 11:42:51 +0545

    created a meeting componenet page

--- resources/js/pages/Meeting.tsx
+++ resources/js/pages/Meeting.tsx
@@ -1,18 +1,24 @@
 import { Head } from '@inertiajs/react';
+import PrimaryButton from '@/Components/PrimaryButton';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 
-export default function Members() {
+export default function Meeting() {
     return (
         <AuthenticatedLayout
             header={
                 <>
                     <h1 className="text-7xl font-semibold leading-tight text-gray-800">
-                        Members
+                        Meeting
                     </h1>
+                    <form>
+                        <PrimaryButton type={'submit'}>
+                            CREATE MEETING
+                        </PrimaryButton>
+                    </form>
                 </>
             }
         >
-            <Head title="Members" />
+            <Head title="Meeting" />
         </AuthenticatedLayout>
     );
 }
