d24238385bd3017ebadcada56418ca2063b41214
acharya-dilip <acharyamission6@gmail.com>
2026-05-21 13:41:07 +0545

    wrote an index function for the dashboard controller which returns the component and passes the posts to the component

--- app/Http/Controllers/dashboard/dashboardController.php
+++ app/Http/Controllers/dashboard/dashboardController.php
@@ -5,9 +5,20 @@ namespace App\Http\Controllers\dashboard;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class dashboardController extends Controller
 {
+    public function index()
+    {
+
+        $posts = Post::latest()->get();
+
+        return Inertia::render('dashboard/Dashboard', [
+            'posts' => $posts,
+        ]);
+    }
+
     public function store(Request $request)
     {
         $valid = request()->validate([
