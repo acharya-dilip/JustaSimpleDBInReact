7a0e64255b23fd784e2164b601cbc04f0b4a0143
acharya-dilip <acharyamission6@gmail.com>
2026-05-21 13:28:02 +0545

    created the model for the post

---
+++ app/Http/Controllers/dashboard/dashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class dashboardController extends Controller
+{
+    function store(Request $request){
+
+
+
+    }
+}
