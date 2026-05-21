631f2ccf7f6b39eb3c355c27a25c9c2a2f8d573a
acharya-dilip <acharyamission6@gmail.com>
2026-05-21 13:31:00 +0545

    updated the post for to also include a title

--- resources/js/pages/dashboard/Create.tsx
+++ resources/js/pages/dashboard/Create.tsx
@@ -16,18 +16,28 @@ export default function Create(){
         >
             <Head title="Create Post" />
 
-            <Container className={'p-8 pb-4 mt-10 pt-4'}>
-                <form method={"post"} action={route('dashboard.store')}>
-                    <h1 className="mb-2 text-5xl font-semibold leading-tight text-white dark:text-gray-200">
+            <Container className={'mt-10 p-8 pb-4 pt-4'}>
+                <form method={'post'}>
+
+                    <h1 className="mb text-5xl font-semibold leading-tight text-white dark:text-gray-200">
+                       Title:
+                    </h1>
+
+                    <TextArea
+                        className={'w-full mb-6'}
+                        name={'title'}
+                    ></TextArea>
+
+                    <h1 className=" text-5xl font-semibold leading-tight text-white dark:text-gray-200">
                         Description:
                     </h1>
 
                     <TextArea
-                        className={'w-full'}
+                        className={'w-full h-52'}
                         name={'description'}
                     ></TextArea>
 
-                    <h1 className="mt-4 text-5xl font-semibold leading-tight text-white mb-2 dark:text-gray-200">
+                    <h1 className="mb-2 mt-4 text-5xl font-semibold leading-tight text-white dark:text-gray-200">
                         Image:
                     </h1>
                     <input
@@ -35,20 +45,17 @@ export default function Create(){
                         accept="image/*"
                         id="image"
                         name="image"
-                        className={"ml-1"}
+                        className={'ml-1'}
                         required
                     />
 
-                    <br/>
+                    <br />
 
-                    <div className={"flex justify-end mt-4"}>
+                    <div className={'mt-4 flex justify-end'}>
                         <PrimaryButton type={'submit'}>
-                            <p className={"text-xl text-black"}>SUBMIT</p>
+                            <p className={'text-xl text-black'}>SUBMIT</p>
                         </PrimaryButton>
                     </div>
-
-
-
                 </form>
             </Container>
         </AuthenticatedLayout>
