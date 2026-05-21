04e6d888c99b84caa3e052a6e02ecdf997af8d44
acharya-dilip <acharyamission6@gmail.com>
2026-05-21 16:36:05 +0545

    created teh members page layout

--- resources/js/pages/members/Members.tsx
+++ resources/js/pages/members/Members.tsx
@@ -1,7 +1,8 @@
 import { Head } from '@inertiajs/react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import Post from '@/Components/Post';
 
-export default function Members() {
+export default function Members({members}:{members:any}) {
     return (
         <AuthenticatedLayout
             header={
@@ -13,6 +14,16 @@ export default function Members() {
             }
         >
             <Head title="Members" />
+
+            <div
+                className={
+                    'grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:grid-cols-4'
+                }
+            >
+                {members.map((member: any) => (
+                    <Post key={member.id} post={member} />
+                ))}
+            </div>
         </AuthenticatedLayout>
     );
 }
