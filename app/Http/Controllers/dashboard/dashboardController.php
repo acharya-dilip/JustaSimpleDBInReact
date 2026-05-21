8808f775ab7bed54eb04cb92d6ed656ecc7a3e3c
acharya-dilip <acharyamission6@gmail.com>
2026-05-21 13:35:26 +0545

    wrote the method to store the post in teh controller

--- app/Http/Controllers/dashboard/dashboardController.php
+++ app/Http/Controllers/dashboard/dashboardController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers\dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class dashboardController extends Controller
 {
-    function store(Request $request){
+    public function store(Request $request)
+    {
+        $valid = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
 
+        $post = new Post;
 
+        $post->title = $valid['title'];
+        $post->description = $valid['description'];
+
+        $post->image_path = $request->file('image')->store('uploads', 'public');
+
+        $post->save();
 
     }
 }
