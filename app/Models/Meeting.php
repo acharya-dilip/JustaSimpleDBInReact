45e35074e69abdfcaaa1bee094994f7a500581db
acharya-dilip <acharyamission6@gmail.com>
2026-05-22 12:20:15 +0545

    wrote the model for the meeting

---
+++ app/Models/Meeting.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Meeting extends Model
+{
+
+    protected $table = 'meetings';
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['agenda','description','time','date','location','latitude','longitude'];
+
+
+
+}
