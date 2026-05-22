ca1965c1cb7d8b97e47e784f8c04f83e0138a0e9
acharya-dilip <acharyamission6@gmail.com>
2026-05-22 12:46:03 +0545

    copied over the create form for meetings and edited it accordingly with reacrt

--- resources/js/pages/dashboard/Create.tsx
+++ resources/js/pages/dashboard/Create.tsx
@@ -3,14 +3,9 @@ import Container from '@/Components/Container';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextArea from '@/Components/TextArea';
+import TextInput from '@/Components/TextInput';
 
 export default function Create(){
-
-    const { data, setData, post, processing } = useForm({
-        title: '',
-        description: '',
-        image: null as File | null,
-    });
 
 
     return (
@@ -26,44 +21,64 @@ export default function Create(){
             <Container className={'mt-10 p-8 pb-4 pt-4'}>
                 <form
                     method={'post'}
-                    action={route('dashboard.store')}
-                    encType={'multipart/form-data'}
+                    action={route('')}
                 >
 
                     {/*Equivalent of a csrf token*/}
 
+                    <div className="grid grid-cols-1 sm:grid-cols-2">
 
-                    <h1 className="mb text-5xl font-semibold leading-tight text-white dark:text-gray-200">
-                        Title:
+                        <div>
+                            <h1 className="font-semibold text-4xl text-gray-800 dark:text-gray-200 leading-tight">
+                                Date:
+                            </h1>
+                        </div>
+                        <div>
+                            <h1 className="font-semibold text-4xl text-gray-800 dark:text-gray-200 leading-tight">
+                                Time:
+                            </h1>
+                        </div>
+                        <div>
+                            <TextInput id={"date"} type={"date"} name={"date"} className={"h-12 mt-1 font-bold text-2xl"}  required></TextInput>
+                        </div>
+                        <div>
+                            <TextInput id={"time" } type={"time"} name={"time"} className={"h-12 mt-1 font-bold text-2xl"} required></TextInput>
+                        </div>
+
+                    </div>
+
+
+                    <h1 className="font-semibold text-4xl text-gray-800 dark:text-gray-200 leading-tight mt-4">
+                        Location:
                     </h1>
+                    <TextInput id={"location"} name={"location"} className={"h-12 w-full mt-1 font-bold text-2xl "} required></TextInput>
 
-                    <TextArea
-                        className={'mb-6 w-full'}
-                        name={'title'}
-                    ></TextArea>
 
-                    <h1 className="text-5xl font-semibold leading-tight text-white dark:text-gray-200">
-                        Description:
+                    <div className="grid grid-cols-1 sm:grid-cols-2">
+
+                        <div>
+                            <h1 className="font-semibold text-4xl text-gray-800 dark:text-gray-200 leading-tight mt-4">
+                                Latitude
+                            </h1>
+                        </div>
+                        <div>
+                            <h1 className="font-semibold text-4xl text-gray-800 dark:text-gray-200 leading-tight mt-4">
+                                Longitude
+                            </h1>
+                        </div>
+                        <div>
+                            <TextInput type={"number"} step={"any"} id={"latitude"} name={"latitude"} className={"h-12 mt-1 font-bold text-2xl"} required></TextInput>
+                        </div>
+                        <div>
+                            <TextInput type={"number"} step={"any"} id={"longitude"} name={"longitude"} className={"h-12 mt-1 font-bold text-2xl"} required></TextInput>
+                        </div>
+                    </div>
+
+                    <h1 className="font-semibold text-4xl text-gray-800 dark:text-gray-200 leading-tight mt-4">
+                        Context:
                     </h1>
+                    <TextArea className={"w-full h-36"} name={"context"} required></TextArea>
 
-                    <TextArea
-                        className={'h-52 w-full'}
-                        name={'description'}
-                    ></TextArea>
-
-                    <h1 className="mb-2 mt-4 text-5xl font-semibold leading-tight text-white dark:text-gray-200">
-                        Image:
-                    </h1>
-                    <input
-                        type="file"
-                        accept="image/*"
-                        id="image"
-                        name="image"
-                        className={'ml-1'}
-                        required
-                    />
-
-                    <br />
 
                     <div className={'mt-4 flex justify-end'}>
                         <input type={'submit'} value={"SUBMIT"} className={"bg-white p-2 rounded-lg font-bold"} />
---
+++ resources/js/pages/meeting/Create.tsx
@@ -0,0 +1,71 @@
+import { Head, useForm } from '@inertiajs/react';
+import Container from '@/Components/Container';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextArea from '@/Components/TextArea';
+
+export default function Create() {
+
+
+    return (
+        <AuthenticatedLayout
+            header={
+                <h1 className="text-7xl font-semibold leading-tight text-white dark:text-gray-200">
+                    Create Post
+                </h1>
+            }
+        >
+            <Head title="Create Post" />
+
+            <Container className={'mt-10 p-8 pb-4 pt-4'}>
+                <form
+                    method={'post'}
+                    action={route('dashboard.store')}
+                    encType={'multipart/form-data'}
+                >
+                    {/*Equivalent of a csrf token*/}
+
+                    <h1 className="mb text-5xl font-semibold leading-tight text-white dark:text-gray-200">
+                        Title:
+                    </h1>
+
+                    <TextArea
+                        className={'mb-6 w-full'}
+                        name={'title'}
+                    ></TextArea>
+
+                    <h1 className="text-5xl font-semibold leading-tight text-white dark:text-gray-200">
+                        Description:
+                    </h1>
+
+                    <TextArea
+                        className={'h-52 w-full'}
+                        name={'description'}
+                    ></TextArea>
+
+                    <h1 className="mb-2 mt-4 text-5xl font-semibold leading-tight text-white dark:text-gray-200">
+                        Image:
+                    </h1>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        id="image"
+                        name="image"
+                        className={'ml-1'}
+                        required
+                    />
+
+                    <br />
+
+                    <div className={'mt-4 flex justify-end'}>
+                        <input
+                            type={'submit'}
+                            value={'SUBMIT'}
+                            className={'rounded-lg bg-white p-2 font-bold'}
+                        />
+                    </div>
+                </form>
+            </Container>
+        </AuthenticatedLayout>
+    );
+}
