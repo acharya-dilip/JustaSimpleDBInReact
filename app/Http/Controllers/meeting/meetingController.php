61a0d24cc0cbc02f5f575f060b25f18b3239eac0
acharya-dilip <acharyamission6@gmail.com>
2026-05-22 12:16:32 +0545

    copied over the meeting controller

---
+++ app/Http/Controllers/meeting/meetingController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\meeting;
+
+use App\Http\Controllers\Controller;
+use App\Models\meeting;
+use Illuminate\Http\Request;
+
+class meetingController extends Controller
+{
+
+
+    function index()
+    {
+
+        $meetings = meeting::latest()->get();
+
+
+
+        return view('meeting.app', compact('meetings'));
+    }
+
+
+    function store(Request $request){
+
+        $validate = $request->validate([
+            'agenda' => 'required',
+            'time' => 'required',
+            'location' => 'required',
+            'date' => 'required',
+            'context' => 'required',
+            'latitude' => 'required',
+            'longitude' => 'required',
+        ]);
+
+        $meeting = new meeting();
+
+        $meeting->agenda = $validate['agenda'];
+        $meeting->time = $validate['time'];
+        $meeting->location = $validate['location'];
+        $meeting->date = $validate['date'];
+        $meeting->latitude = $validate['latitude'];
+        $meeting->longitude = $validate['longitude'];
+        $meeting->context = $validate['context'];
+
+        $meeting->save();
+
+        return redirect(route('meeting.index'));
+
+    }
+}
